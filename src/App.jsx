fb9380019457f14d6fa6f27bb10b6de323ec485c
Remi patidar <rameshpatidar@itinformatix.com>
2023-10-05 15:23:35 +0530

    done events used

--- src/App.jsx
+++ src/App.jsx
@@ -23,24 +23,39 @@ import React, { useState } from "react";
 const App = () => {
     //hookes used
     const [name, setname] = useState("");
+
     const [fullname, setFullName] = useState();
+    const [lastname, setlastname] = useState("");
+    const [lastnamenew, setlastnamenew] = useState("");
+
     const inputEvent = (event) => {
-        // console.log(event.target.value);
+        // console.log(event.target.value);                
         //event used
         setname(event.target.value)
     };
-    const onSubmit = () => {
-        setFullName(name);
+    const onSubmit = (event) => {
+        event.preventDefault()
+        setFullName(name);       
+        setlastnamenew(lastname);
     };
+    const inputEventtwo = (event) => {            
+        setlastname(event.target.value)
+    }
     return (
         <>
-            <div>
-                <h1>Hello {fullname}</h1>
-                <input type="text" placeholder="please fill your name" onChange={inputEvent} value={name}></input>
-                <button onClick={onSubmit}>click me 👍 </button>
+            <div className="main_div">
+                    <div>
+                <form onSubmit={onSubmit}> 
+                        <h1>Login {fullname} {lastnamenew} </h1>
+                        <input type="text" placeholder="Enter your full name"
+                            onChange={inputEvent} value={name} />
+                        <input type="text" placeholder="Enter your last name"
+                            onChange={inputEventtwo} value={lastname} />
+                        <button type="submit">Submit 👍 </button>
+                </form>
+                    </div>
             </div>
         </>
     )
-
 }
 export default App;  
